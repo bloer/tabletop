a56fc24ff0a22506354918f1232c666fb8decfe9
Ben Loer <bloer@fnal.gov>
2015-01-25 11:11:43 -0600

    added bump_version.sh

---
+++ bump_version.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+print_usage(){
+    echo "$0 <newversion>"
+    exit 0
+}
+
+NEWVERSION="$1"
+[ -n "$NEWVERSION" ] || print_usage
+
+OLDVERSION=`cat VERSION`
+#escape periods
+OLDVERSION=${OLDVERSION//./\.}
+
+FILELIST="package.json server.js static/index.html static/tabletopclient.js"
+for file in $FILELIST ; do
+    sed "s/$OLDVERSION/$NEWVERSION/g" $file >${file}.tmp
+    mv ${file}.tmp $file
+done
